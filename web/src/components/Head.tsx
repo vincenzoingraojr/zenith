6b2c99bb0e82671d50e02a5a2ce9d4e72f410089
Vincenzo Ingrao Jr. <vincenzoingraojr@gmail.com>
2025-04-27 14:57:36 +0200

    fix: theme-color for the PWA

--- web/src/components/Head.tsx
+++ web/src/components/Head.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { Helmet } from "react-helmet";
+import { useTheme } from "../styles/theme";
 
 interface HeadProps {
     title: string;
@@ -10,6 +11,7 @@ interface HeadProps {
 const Head: FunctionComponent<HeadProps> = ({ title, description, image }) => {
     const defaultDescription = "The everything app. Find out what's happening in the world in real time, chat and make video calls with whoever you want. Send and receive money.";
     const defaultImage = "https://img.zncdn.net/brand/logo.png";
+    const theme = useTheme();
 
     return (
         <Helmet>
@@ -18,6 +20,7 @@ const Head: FunctionComponent<HeadProps> = ({ title, description, image }) => {
             <meta name="description" content={description ? description : defaultDescription} />
             <meta property="og:description" content={description ? description : defaultDescription} />
             <meta property="og:image" content={image ? image : defaultImage} />
+            <meta name="theme-color" content={theme.background} />
         </Helmet>
     );
 };
