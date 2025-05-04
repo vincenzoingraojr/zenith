a0e84bc5c4e2a556c51d7f3a9539c680add181fd
Vincenzo Ingrao Jr. <vincenzoingraojr@gmail.com>
2025-05-05 00:23:12 +0200

    Testint the options component

--- web/src/components/icons/Magnifier.tsx
+++ web/src/components/icons/Magnifier.tsx
@@ -1,17 +1,28 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import { DynamicSizeIcon, IconProps } from "./common";
 import { COLORS } from "../../styles/colors";
 import { useTheme } from "../../styles/theme";
 
 interface MagnifierProps extends IconProps {
-    type?: "normal" | "small";
+    type?: "normal" | "options" | "small";
 }
 
 const Magnifier: FunctionComponent<MagnifierProps> = ({ type, isActive }) => {
     const theme = useTheme();
+    const [size, setSize] = useState(26);
+
+    useEffect(() => {
+        if (type === "normal") {
+            setSize(26);
+        } else if (type === "options") {
+            setSize(24);
+        } else {
+            setSize(18);
+        }
+    }, [type]);
 
     return (
-        <DynamicSizeIcon size={type === "normal" ? 26 : 18} hasStroke={isActive ? COLORS.blue : theme.color}>
+        <DynamicSizeIcon size={size} hasStroke={isActive ? COLORS.blue : theme.color}>
             {isActive ? (
                 <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M14.8593 14.8593L20 20M14.8593 14.8593C17.3435 12.3751 17.3435 8.34739 14.8593 5.86317C12.3751 3.37894 8.34739 3.37894 5.86317 5.86317C3.37894 8.34739 3.37894 12.3751 5.86317 14.8593C8.34739 17.3435 12.3751 17.3435 14.8593 14.8593Z" strokeWidth="3" strokeLinecap="round" />
--- web/src/pages/search/SearchPage.tsx
+++ web/src/pages/search/SearchPage.tsx
@@ -1,9 +1,15 @@
 import Head from "../../components/Head";
+import Magnifier from "../../components/icons/Magnifier";
+import Settings from "../../components/icons/Settings";
 import PageLayout from "../../components/layouts/PageLayout";
 import PageContentLayout from "../../components/layouts/sublayouts/PageContentLayout";
+import Options, { OptionItem, OptionItemIcon, OptionItemText } from "../../components/Options";
+import { useOptions } from "../../components/utils/hooks";
 import { PageText } from "../../styles/global";
 
 function SearchPage() {
+    const { activeOptions, handleOptionsClick } = useOptions();
+
     return (
         <>
             <Head 
@@ -20,6 +26,29 @@ function SearchPage() {
                             <>
                                 <PageText>Search on Zenith.</PageText>
                             </>
+                        }
+                        headerIconsComponent={
+                            <Options
+                                key={0}
+                                title="Search options" 
+                                icon={<Settings type="options" />}
+                                isOpen={activeOptions === 0}
+                                toggleOptions={() =>
+                                    handleOptionsClick(0)
+                                }
+                                children={
+                                    <>
+                                        <OptionItem>
+                                            <OptionItemIcon>
+                                                <Magnifier type="options" isActive={false} />
+                                            </OptionItemIcon>
+                                            <OptionItemText>
+                                                Advanced search
+                                            </OptionItemText>
+                                        </OptionItem>
+                                    </>
+                                }
+                            />
                         }
                     />
                 }
